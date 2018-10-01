1eb00922c343ead3c6b176f7e0ae77a191d0a4c2
Robert Rothenberg <rrwo@cpan.org>
2018-10-01 23:57:41 +0100

    Bump version in README

--- lib/Graphics/ColorNames/Windows.pm
+++ lib/Graphics/ColorNames/Windows.pm
@@ -23,7 +23,7 @@ C<Graphics::ColorNames>
 use strict;
 use warnings;
 
-our $VERSION = 'v3.1.3';
+our $VERSION = 'v3.2.0';
 
 sub NamesRgbTable() {
     use integer;
--- lib/Graphics/ColorNames/X.pm
+++ lib/Graphics/ColorNames/X.pm
@@ -23,7 +23,7 @@ L<Graphics::ColorNames>, F<rgb.txt>
 use strict;
 use warnings;
 
-our $VERSION = 'v3.1.3';
+our $VERSION = 'v3.2.0';
 
 # Original name and colors (v1.01 and earlier) from Kevin J. Walsh,
 # http://eies.njit.edu/~walsh/rgb.html
