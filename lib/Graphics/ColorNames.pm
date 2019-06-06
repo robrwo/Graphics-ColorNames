12654b925dd89652ece4ace1a8f4733c32a3c946
Robert Rothenberg <rrwo@cpan.org>
2019-06-06 21:12:53 +0100

    Bump VERSION to v3.5.0

--- lib/Graphics/ColorNames.pm
+++ lib/Graphics/ColorNames.pm
@@ -15,7 +15,7 @@ use File::Spec::Functions qw/ file_name_is_absolute /;
 use Module::Load 0.10;
 use Module::Loaded;
 
-our $VERSION = 'v3.4.1';
+our $VERSION = 'v3.5.0';
 
 our %EXPORT_TAGS = (
     'all'     => [qw( hex2tuple tuple2hex all_schemes )],
